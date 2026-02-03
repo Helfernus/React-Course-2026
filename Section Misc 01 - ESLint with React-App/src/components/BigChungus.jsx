2a3e7bc7a5b7283e9e3558301001feb01a49c4a4
Helfernus <35947615+Helfernus@users.noreply.github.com>
2026-02-03 13:33:30 +0530

    Initial Commit

---
+++ Section Misc 01 - ESLint with React-App/src/components/BigChungus.jsx
@@ -0,0 +1,22 @@
+import { Fragment, useEffect } from 'react';
+
+export default function BigChungus() {
+  var Hello = <Hello personal />;
+
+  console.log('Log a debug level message.');
+console.warn('Log a warn level message.');
+console.error('Log an error level message.');
+
+const a = 3;
+console.log(a);
+  let asd;
+  function foo() {
+    if (true) {
+      useEffect(()=> {}, []);
+    }
+  }
+  return <>
+  <p>Hola Mundo!</p>
+  <div>You</div>
+  </>;
+}
